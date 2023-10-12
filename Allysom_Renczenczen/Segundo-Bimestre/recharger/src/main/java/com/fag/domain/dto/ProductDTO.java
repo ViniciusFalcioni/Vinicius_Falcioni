fd8f8dea3f448fa22812439fc36b95db4c2515c3
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-10-11 23:35:22 -0300

    Adicionando atributos e getters and setters para classe ProductDTO

--- Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/ProductDTO.java
+++ Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/ProductDTO.java
@@ -1,4 +1,31 @@
 package com.fag.domain.dto;
 
 public class ProductDTO {
+    String name;
+    Double value;
+    Integer due;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Integer getDue() {
+        return due;
+    }
+
+    public void setDue(Integer due) {
+        this.due = due;
+    }
 }
