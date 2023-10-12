886860c4ffba6a282e6b423101af0a14b2abdc7d
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-10-11 23:33:21 -0300

    Adicionando atributos e gettter and setter na classe OperatorDTO

--- Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/OperatorDTO.java
+++ Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/OperatorDTO.java
@@ -1,4 +1,39 @@
 package com.fag.domain.dto;
 
 public class OperatorDTO {
+    Integer category;
+    String name;
+    Integer providerId;
+    Double maxValue;
+    Double minValue;
+    public Integer getCategory() {
+        return category;
+    }
+    public void setCategory(Integer category) {
+        this.category = category;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getProviderId() {
+        return providerId;
+    }
+    public void setProviderId(Integer providerId) {
+        this.providerId = providerId;
+    }
+    public Double getMaxValue() {
+        return maxValue;
+    }
+    public void setMaxValue(Double maxValue) {
+        this.maxValue = maxValue;
+    }
+    public Double getMinValue() {
+        return minValue;
+    }
+    public void setMinValue(Double minValue) {
+        this.minValue = minValue;
+    }
 }
