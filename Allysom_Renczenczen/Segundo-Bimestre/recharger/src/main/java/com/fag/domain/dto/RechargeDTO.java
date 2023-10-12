20033d6212ec720138ea209fe2503c9feda69ecc
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-10-11 23:36:40 -0300

    Adicionando atributos e getters and setters para classe Recharge DTO

--- Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/RechargeDTO.java
+++ Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/RechargeDTO.java
@@ -1,4 +1,76 @@
 package com.fag.domain.dto;
 
 public class RechargeDTO {
+    String id;
+    Double value;
+    String document;
+    Integer operatorId;
+    PhoneDTO phone;
+    String receipt;
+    Long transactionId;
+    boolean sucess;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public Integer getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Integer operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public PhoneDTO getPhone() {
+        return phone;
+    }
+
+    public void setPhone(PhoneDTO phone) {
+        this.phone = phone;
+    }
+
+    public String getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(String receipt) {
+        this.receipt = receipt;
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public boolean isSucess() {
+        return sucess;
+    }
+
+    public void setSucess(boolean sucess) {
+        this.sucess = sucess;
+    }
 }
