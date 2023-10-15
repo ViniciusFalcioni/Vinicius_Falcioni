1f0039ddd9dab4f2ab691d6075b6e65874e0d74b
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-15 18:23:48 -0300

    Ajuste na documentação do IMapper

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/interfaces/IMapper.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/interfaces/IMapper.java
@@ -1,27 +1,29 @@
 package com.fag.domain.interfaces;
 
 /**
- * A interface IMapper é responsável por definir os métodos para realizar a conversão
- * entre um DTO (Data Transfer Object) e um BO (Business Object).
+ * A interface IMapper é responsável por definir os métodos para realizar a
+ * conversão entre um DTO (Data Transfer Object) e um BO (Business Object).
  *
- * @param <DTO> O tipo de objeto DTO que será mapeado.
- * @param <BO> O tipo de objeto BO resultante após o mapeamento.
+ * @param <DTO> O tipo de objeto DTO que será mapeado e retornado, de acordo
+ *              com o método executado.
+ * @param <BO>  O tipo de objeto BO que será mapeado e retornado, de acordo
+ *              com o método executado.
  */
 public interface IMapper<DTO, BO> {
 
     /**
-     * Realiza o mapeamento de um objeto DTO para um objeto BO.
+     * Realiza o mapeamento de um DTO para um BO.
      *
      * @param dto O objeto DTO a ser mapeado para um objeto BO.
-     * @return O objeto BO resultante após o mapeamento.
+     * @return O objeto BO correspondente ao DTO mapeado.
      */
     BO toBO(DTO dto);
 
     /**
-     * Realiza o mapeamento de um objeto BO para um objeto DTO.
+     * Realiza o mapeamento de um BO para um DTO.
      *
      * @param entity O objeto BO a ser mapeado para um objeto DTO.
-     * @return O objeto DTO resultante após o mapeamento.
+     * @return O objeto DTO correspondente ao BO mapeado.
      */
     DTO toDTO(BO entity);
 
