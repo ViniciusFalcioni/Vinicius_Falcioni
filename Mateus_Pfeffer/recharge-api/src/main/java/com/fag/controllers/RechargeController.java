7013e1a16c5f403b4150132a4951fddf06da1ad6
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-23 19:29:39 -0300

    Adicionado endpoints para realizar a criação, atualização, aprovação e rejeição de um Recharge

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/controllers/RechargeController.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/controllers/RechargeController.java
@@ -1,29 +1,46 @@
 package com.fag.controllers;
 
+import com.fag.domain.dto.RechargeDTO;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/recharge")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class RechargeController {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public Response listRecharges() {
         return Response.ok().build();
     }
 
     @GET
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getRecharge(@PathParam("id") String id) {
         return Response.ok().build();
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response createRecharge() {
+    public Response createRecharge(RechargeDTO dto) {
+        return Response.ok(dto).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateRecharge(@PathParam("id") String rechargeId, RechargeDTO dto) {
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("/{id}/approve")
+    public Response approveRecharge(@PathParam("id") String rechargeId) {
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("/{id}/refuse")
+    public Response refuseRecharge(@PathParam("id") String rechargeId) {
         return Response.ok().build();
     }
 
