961ef23f36e3f47115b7c51c2b0c18b2280f6bdf
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-23 19:28:08 -0300

    Adicionada anotação @QuarkusTest

--- Mateus_Pfeffer/recharge-api/src/test/java/com/fag/controllers/ProductControllerTest.java
+++ Mateus_Pfeffer/recharge-api/src/test/java/com/fag/controllers/ProductControllerTest.java
@@ -1,9 +1,11 @@
 package com.fag.controllers;
 
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@QuarkusTest
 class ProductControllerTest {
 
     @Test
