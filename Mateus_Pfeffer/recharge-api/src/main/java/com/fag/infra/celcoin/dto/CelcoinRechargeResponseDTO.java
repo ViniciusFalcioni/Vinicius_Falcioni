30cb3645a92f24be4bdab419d5ccdeb489b92a0e
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 22:04:54 -0300

    Adicionado campo success ao CelcoinRechargeResponseDTO

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/dto/CelcoinRechargeResponseDTO.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/dto/CelcoinRechargeResponseDTO.java
@@ -1,4 +1,4 @@
 package com.fag.infra.celcoin.dto;
 
-public record CelcoinRechargeResponseDTO(Integer nsuNameProvider, Integer authentication, CelcoinRechargeReceiptDTO receipt, String settleDate, String createDate, Long transactionId, String urlReceipt, String errorCode, String message, Integer status) {
+public record CelcoinRechargeResponseDTO(Integer nsuNameProvider, Integer authentication, CelcoinRechargeReceiptDTO receipt, String settleDate, String createDate, Long transactionId, String urlReceipt, String errorCode, String message, Integer status, boolean success) {
 }
