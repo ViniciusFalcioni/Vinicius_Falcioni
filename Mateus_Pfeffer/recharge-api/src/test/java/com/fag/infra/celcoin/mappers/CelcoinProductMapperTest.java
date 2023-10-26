300fd30efc053fc8fb332b93c97239afc3d7c4b5
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-26 20:02:51 -0300

    Criação da estrutura inicial de testes para a camada de infra

---
+++ Mateus_Pfeffer/recharge-api/src/test/java/com/fag/infra/celcoin/mappers/CelcoinProductMapperTest.java
@@ -0,0 +1,13 @@
+package com.fag.infra.celcoin.mappers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CelcoinProductMapperTest {
+
+    @Test
+    void toAppDTO() {
+    }
+
+}
---
+++ Mateus_Pfeffer/recharge-api/src/test/java/com/fag/infra/celcoin/mappers/CelcoinRechargeMapperTest.java
@@ -0,0 +1,17 @@
+package com.fag.infra.celcoin.mappers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CelcoinRechargeMapperTest {
+
+    @Test
+    void toAppDTO() {
+    }
+
+    @Test
+    void toVendorDTO() {
+    }
+
+}
