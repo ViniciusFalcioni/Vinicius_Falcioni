242ddb1c626422bc7fa419a6212244db266e2916
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 21:49:30 -0300

    Criação do CelcoinRechargeMapper

---
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinRechargeMapper.java
@@ -0,0 +1,16 @@
+package com.fag.infra.celcoin.mappers;
+
+import com.fag.domain.dto.RechargeDTO;
+import com.fag.infra.celcoin.dto.CelcoinRechargeDTO;
+
+public class CelcoinRechargeMapper {
+
+    public static RechargeDTO toAppDTO(CelcoinRechargeDTO vendor) {
+        return null;
+    }
+
+    public static CelcoinRechargeDTO toVendorDTO(RechargeDTO appDTO) {
+        return null;
+    }
+
+}
