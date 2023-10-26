158eee7070d2bbcf405f6194ef467c1f2edde805
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 21:50:55 -0300

    Ajuste ListProducts, interface para propriedade

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/usecases/ListProducts.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/usecases/ListProducts.java
@@ -3,31 +3,19 @@ package com.fag.domain.usecases;
 import com.fag.domain.dto.OperatorDTO;
 import com.fag.domain.dto.ProductDTO;
 import com.fag.domain.dto.RechargeDTO;
-import com.fag.domain.repositories.IRechargeVendorRepository;
+import com.fag.domain.repositories.IRechargeVendor;
 
 import java.util.List;
 
-public class ListProducts implements IRechargeVendorRepository {
+public class ListProducts {
 
-    public ListProducts() {
+    private IRechargeVendor vendor;
+
+    public ListProducts(IRechargeVendor vendor) {
+        this.vendor = vendor;
     }
 
-    public List<ProductDTO> execute(Integer operatorId) {
-        return null;
-    }
-
-    @Override
-    public RechargeDTO create(RechargeDTO recharge) {
-        return null;
-    }
-
-    @Override
-    public List<OperatorDTO> listOperators(Integer stateCode, Integer category) {
-        return null;
-    }
-
-    @Override
-    public List<ProductDTO> listProducts(Integer stateCode, Integer operatorId) {
+    public List<ProductDTO> execute(Integer stateCode, Integer operatorId) {
         return null;
     }
 
