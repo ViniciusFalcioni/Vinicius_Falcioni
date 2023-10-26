2f3b8dfebf1dd2f8b2231d755c45eeef9378bf23
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 22:04:12 -0300

    Ajuste no RechargeMapper para utilizar o construtor

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/mappers/RechargeMapper.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/mappers/RechargeMapper.java
@@ -2,7 +2,6 @@ package com.fag.domain.mappers;
 
 import com.fag.domain.dto.PhoneDTO;
 import com.fag.domain.dto.RechargeDTO;
-import com.fag.domain.entities.PhoneBO;
 import com.fag.domain.entities.RechargeBO;
 
 import java.util.UUID;
@@ -10,20 +9,15 @@ import java.util.UUID;
 public class RechargeMapper {
 
     public static RechargeBO toBO(RechargeDTO dto) {
-        RechargeBO newRechargeBO = new RechargeBO();
-        newRechargeBO.setId(UUID.fromString(dto.id()));
-        newRechargeBO.setValue(dto.value());
-        newRechargeBO.setDocument(dto.document());
-        newRechargeBO.setProviderId(dto.operatorId());
-
-        PhoneBO phone = PhoneMapper.toBO(dto.phone());
-
-        newRechargeBO.setPhone(phone);
-        newRechargeBO.setReceipt(dto.receipt());
-        newRechargeBO.setTransactionId(dto.transactionId());
-        newRechargeBO.setSuccess(dto.success());
-
-        return newRechargeBO;
+        return new RechargeBO(
+                UUID.fromString(dto.id()),
+                dto.value(),
+                dto.document(),
+                dto.operatorId(),
+                PhoneMapper.toBO(dto.phone()),
+                dto.receipt(),
+                dto.transactionId(),
+                dto.success());
 
     }
 
