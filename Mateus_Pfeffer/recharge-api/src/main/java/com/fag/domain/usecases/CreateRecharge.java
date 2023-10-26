7a7312f6ff3dec80f8ad2ed15fc4c279e640d25f
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 21:50:05 -0300

    Ajuste no CreateRecharge, interfaces alteradas para propriedades

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/usecases/CreateRecharge.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/usecases/CreateRecharge.java
@@ -5,36 +5,21 @@ import com.fag.domain.dto.ProductDTO;
 import com.fag.domain.dto.RechargeDTO;
 import com.fag.domain.entities.RechargeBO;
 import com.fag.domain.repositories.IRechargeDataBaseRepository;
-import com.fag.domain.repositories.IRechargeVendorRepository;
+import com.fag.domain.repositories.IRechargeVendor;
 
 import java.util.List;
 
-public class CreateRecharge implements IRechargeVendorRepository, IRechargeDataBaseRepository {
+public class CreateRecharge {
 
-    public CreateRecharge() {
+    private IRechargeVendor vendor;
+    private IRechargeDataBaseRepository dbRepository;
+
+    public CreateRecharge(IRechargeVendor vendor, IRechargeDataBaseRepository dbRepository) {
+        this.vendor = vendor;
+        this.dbRepository = dbRepository;
     }
 
     public RechargeDTO execute(RechargeDTO dto) {
-        return null;
-    }
-
-    @Override
-    public RechargeBO persist(RechargeBO bo) {
-        return null;
-    }
-
-    @Override
-    public RechargeDTO create(RechargeDTO recharge) {
-        return null;
-    }
-
-    @Override
-    public List<OperatorDTO> listOperators(Integer stateCode, Integer category) {
-        return null;
-    }
-
-    @Override
-    public List<ProductDTO> listProducts(Integer stateCode, Integer operatorId) {
         return null;
     }
 
