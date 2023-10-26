3328a151070f2dd56ec79ce86ca77049b23aac5e
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 22:03:35 -0300

    Adicionado métodos para setar um valor ao record

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/dto/RechargeDTO.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/dto/RechargeDTO.java
@@ -1,4 +1,17 @@
 package com.fag.domain.dto;
 
 public record RechargeDTO(String id, Double value, String document, Integer operatorId, PhoneDTO phone, String receipt, Long transactionId, boolean success) {
+
+    public RechargeDTO withReceipt(String receipt) {
+        return new RechargeDTO(id(), value(), document(), operatorId(), phone(), receipt, transactionId(), success());
+    }
+
+    public RechargeDTO withTransactionId(Long transactionId) {
+        return new RechargeDTO(id(), value(), document(), operatorId(), phone(), receipt(), transactionId, success());
+    }
+
+    public RechargeDTO withSuccess(boolean success) {
+        return new RechargeDTO(id(), value(), document(), operatorId(), phone(), receipt(), transactionId(), success);
+    }
+
 }
