6a57a22e40c6b83e9f5ebac34a2fd4ea764ebf03
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-25 21:56:07 -0300

    Implementação inicial do método create do RechargeCelcoin

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/repository/RechargeCelcoin.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/repository/RechargeCelcoin.java
@@ -4,16 +4,33 @@ import com.fag.domain.dto.OperatorDTO;
 import com.fag.domain.dto.ProductDTO;
 import com.fag.domain.dto.RechargeDTO;
 import com.fag.domain.repositories.IRechargeVendor;
+import com.fag.infra.celcoin.dto.*;
+import com.fag.infra.celcoin.mappers.CelcoinRechargeMapper;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Form;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.util.List;
 
 @ApplicationScoped
 public class RechargeCelcoin implements IRechargeVendor {
 
+    @Inject
+    @RestClient
+    RestClientCelcoin restClient;
+
     @Override
     public RechargeDTO create(RechargeDTO recharge) {
-        return null;
+        try {
+            CelcoinRechargeDTO rechargeDTO = CelcoinRechargeMapper.toVendorDTO(recharge);
+
+            CelcoinRechargeResponseDTO response = restClient.handleRecharge(getToken(), rechargeDTO);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro comunicação provedor servico recarga.");
+        }
+
+        return recharge;
     }
 
     @Override
@@ -24,6 +41,18 @@ public class RechargeCelcoin implements IRechargeVendor {
     @Override
     public List<ProductDTO> listProducts(Integer stateCode, Integer operatorId) {
         return null;
+    }
+
+    private String getToken() {
+        Form form = new Form();
+
+        form.param("client_id", "");
+        form.param("grant_type", "client_credentials");
+        form.param("client_secret", "");
+
+        CelcoinTokenDTO tokenDTO = restClient.generateToken(form);
+
+        return "Beader " + tokenDTO.accessToken();
     }
 
 }
