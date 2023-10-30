fd3927e654836a279569f28097f0b08a3b7f51e3
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-10-30 13:33:52 -0300

    Adicionando mudnanças

--- Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/infra/celcoin/dto/CelcoinPhoneDTO.java
+++ Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/infra/celcoin/dto/CelcoinPhoneDTO.java
@@ -1,2 +1,31 @@
-package com.fag.infra.celcoin.dto;public class CelcoinPhoneDTO {
+package com.fag.infra.celcoin.dto;
+
+public class CelcoinPhoneDTO {
+    private Integer stateCode;
+    private Integer countryCode;
+    private String Number;
+
+    public Integer getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(Integer stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public Integer getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(Integer countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
 }
