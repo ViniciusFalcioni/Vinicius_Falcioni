9fe0524ce88c859fd2deb7ba16c75cca76bd0f93
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-10-30 14:11:19 -0300

    Refatorando classe product e implementando CelcoinProductMapper

--- Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/ProductDTO.java
+++ Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/domain/dto/ProductDTO.java
@@ -1,9 +1,35 @@
 package com.fag.domain.dto;
 
 public class ProductDTO {
-    String name;
-    Double value;
-    Integer due;
+    private Integer code;
+    private Integer cost;
+    private Integer dueProduct;
+    private String name;
+    private Double value;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Integer getDueProduct() {
+        return dueProduct;
+    }
+
+    public void setDueProduct(Integer dueProduct) {
+        this.dueProduct = dueProduct;
+    }
 
     public String getName() {
         return name;
@@ -19,13 +45,5 @@ public class ProductDTO {
 
     public void setValue(Double value) {
         this.value = value;
-    }
-
-    public Integer getDue() {
-        return due;
-    }
-
-    public void setDue(Integer due) {
-        this.due = due;
     }
 }
