fbc1e218b895f9a4285d2870ace4dca60155f48b
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-10-30 13:42:03 -0300

    Adiconando RechargeReceiptDTO e implementando getters and setters

---
+++ Allysom_Renczenczen/Segundo-Bimestre/recharger/src/main/java/com/fag/infra/celcoin/dto/CelcoinRechargeReceiptDTO.java
@@ -0,0 +1,22 @@
+package com.fag.infra.celcoin.dto;
+
+public class CelcoinRechargeReceiptDTO {
+    private String receiptData;
+    private String receiptformated;
+
+    public String getReceiptData() {
+        return receiptData;
+    }
+
+    public void setReceiptData(String receiptData) {
+        this.receiptData = receiptData;
+    }
+
+    public String getReceiptformated() {
+        return receiptformated;
+    }
+
+    public void setReceiptformated(String receiptformated) {
+        this.receiptformated = receiptformated;
+    }
+}
