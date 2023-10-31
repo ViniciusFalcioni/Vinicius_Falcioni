047b273fb790081b24c7b660070e8dca629ca321
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-31 20:54:10 -0300

    Classes BO alteradas para Record

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/entities/PhoneBO.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/entities/PhoneBO.java
@@ -1,39 +1,4 @@
 package com.fag.domain.entities;
 
-public class PhoneBO {
-
-    Integer stateCode;
-    Integer countryCode;
-    String number;
-
-    public PhoneBO(Integer countryCode, Integer stateCode, String number) {
-        this.countryCode = countryCode;
-        this.stateCode = stateCode;
-        this.number = number;
-    }
-
-    public Integer getStateCode() {
-        return stateCode;
-    }
-
-    public void setStateCode(Integer stateCode) {
-        this.stateCode = stateCode;
-    }
-
-    public Integer getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(Integer countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
+public record PhoneBO(Integer countryCode, Integer stateCode, String number) {
 }
