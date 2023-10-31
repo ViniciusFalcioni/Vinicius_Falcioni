dbfd3ef15123778d001ad052f95635952f6fe028
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-31 20:56:36 -0300

    Renomeação de interface

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/repository/IRestClientCelcoin.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/repository/IRestClientCelcoin.java
@@ -3,18 +3,18 @@ package com.fag.infra.celcoin.repository;
 import com.fag.infra.celcoin.dto.*;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Form;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedHashMap;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @ApplicationScoped
 @RegisterRestClient(baseUri = "https://sandbox.openfinance.celcoin.dev")
-public interface RestClientCelcoin {
+public interface IRestClientCelcoin {
 
     @POST
     @Path("/v5/token")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    CelcoinTokenDTO generateToken(Form form);
+    CelcoinTokenDTO generateToken(MultivaluedHashMap<String, String> form);
 
     @GET
     @Path("/v5/transactions/topups/providers")
