01a99e2af02c408ae1a7d60edbc137ceaf75fbdc
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-10-31 20:55:42 -0300

    Implementação do CelcoinRechargeMapper

--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/dto/CelcoinProductsDTO.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/dto/CelcoinProductsDTO.java
@@ -3,5 +3,4 @@ package com.fag.infra.celcoin.dto;
 import java.util.List;
 
 public record CelcoinProductsDTO(List<CelcoinProductDTO> products) {
-
 }
--- Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinRechargeMapper.java
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/infra/celcoin/mappers/CelcoinRechargeMapper.java
@@ -2,15 +2,28 @@ package com.fag.infra.celcoin.mappers;
 
 import com.fag.domain.dto.RechargeDTO;
 import com.fag.infra.celcoin.dto.CelcoinRechargeDTO;
+import com.fag.infra.celcoin.dto.CelcoinRechargeValueDTO;
+
+import java.util.UUID;
 
 public class CelcoinRechargeMapper {
 
     public static RechargeDTO toAppDTO(CelcoinRechargeDTO vendor) {
-        return null;
+        return new RechargeDTO(UUID.randomUUID().toString(),
+                vendor.topupData().value(),
+                vendor.cpfCnpj(),
+                vendor.providerId(),
+                CelcoinRechargePhoneMapper.toAppDTO(vendor.phone()),
+                null,
+                null,
+                false);
     }
 
     public static CelcoinRechargeDTO toVendorDTO(RechargeDTO appDTO) {
-        return null;
+        return new CelcoinRechargeDTO(appDTO.document(),
+                appDTO.operatorId(),
+                CelcoinRechargePhoneMapper.toVendorDTO(appDTO.phone()),
+                new CelcoinRechargeValueDTO(appDTO.value()));
     }
 
 }
