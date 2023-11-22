b45519e2d5a5a79180fb7610a5c3808b09be3b6c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-21 21:26:32 -0300

    Criação do usecase CreatePixQRCode

---
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/domain/usecases/CreatePixQRCode.java
@@ -0,0 +1,30 @@
+package com.fag.domain.usecases;
+
+import com.fag.domain.dto.PixDTO;
+import com.fag.domain.entities.PixBO;
+import com.fag.domain.mappers.PixMapper;
+import com.fag.domain.repositories.IPixDataBaseRepository;
+import com.fag.domain.repositories.IPixVendor;
+
+public class CreatePixQRCode {
+
+    private IPixVendor vendor;
+
+    private IPixDataBaseRepository dbRepository;
+
+    public CreatePixQRCode(IPixVendor vendor, IPixDataBaseRepository dbRepository) {
+        this.vendor = vendor;
+        this.dbRepository = dbRepository;
+    }
+
+    public PixDTO execute(PixDTO dto) {
+        PixBO bo = PixMapper.toBO(dto);
+
+        PixDTO response = vendor.create(dto);
+
+        dbRepository.persist(bo);
+
+        return response;
+    }
+
+}
