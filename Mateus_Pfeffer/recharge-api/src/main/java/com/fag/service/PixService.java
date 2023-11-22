2f81eb5e6239171b49778d01f65fc7ab029a02fd
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-21 21:27:29 -0300

    Implementação do PixService

---
+++ Mateus_Pfeffer/recharge-api/src/main/java/com/fag/service/PixService.java
@@ -0,0 +1,30 @@
+package com.fag.service;
+
+import com.fag.domain.dto.PixDTO;
+import com.fag.domain.repositories.IPixDataBaseRepository;
+import com.fag.domain.usecases.CreatePixQRCode;
+import com.fag.infra.celcoin.repository.PixCelcoin;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.core.Response;
+
+@ApplicationScoped
+public class PixService {
+
+    @Inject
+    PixCelcoin celcoin;
+
+    @Inject
+    IPixDataBaseRepository repository;
+
+    @Transactional
+    public Response handlePix(PixDTO dto) {
+        CreatePixQRCode createPixQRCode = new CreatePixQRCode(celcoin, repository);
+
+        PixDTO newPix = createPixQRCode.execute(dto);
+
+        return Response.ok(newPix).build();
+    }
+
+}
