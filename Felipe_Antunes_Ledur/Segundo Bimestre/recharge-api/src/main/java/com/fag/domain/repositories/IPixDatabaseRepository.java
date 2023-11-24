12856f37bc215d86ebd3c69f226f29b1cd1d780f
FelipeLedur <felps5000@gmail.com>
2023-11-24 16:04:33 -0300

    Revert "Api pix"
    This reverts commit f921dd6448206c502edda42b70ee43ef4f6e647f.

--- Felipe_Antunes_Ledur/Segundo Bimestre/recharge-api/src/main/java/com/fag/domain/repositories/IPixDatabaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fag.domain.repositories;
-
-import com.fag.domain.entities.PixBO;
-
-public interface IPixDatabaseRepository {
-
-  PixBO persist(PixBO pixDTO);
-  
-}
--- Felipe_Antunes_Ledur/Segundo Bimestre/recharge-api/src/main/java/com/fag/domain/repositories/IPixVendor.java
+++
@@ -1,9 +0,0 @@
-package com.fag.domain.repositories;
-
-import com.fag.domain.dto.PixDTO;
-
-public interface IPixVendor {
-
-  PixDTO create(PixDTO pix);
-
-}
