a8950097e886fee7b34f11dda3a5527b14fdfc85
GabryelFreitasDev <gabryelfreitas04@gmail.com>
2023-11-24 22:17:31 -0300

    API totalmente funcional, últimas refatorações

--- Gabryel_Alves_De_Freitas/SegundoBimestre/pix-api/src/main/java/com/fag/infra/panache/model/PanachePixCode.java
+++ Gabryel_Alves_De_Freitas/SegundoBimestre/pix-api/src/main/java/com/fag/infra/panache/model/PanachePixCode.java
@@ -8,7 +8,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "AKT_PIX_CODE")
 public class PanachePixCode extends PanacheEntityBase {
@@ -27,45 +31,5 @@ public class PanachePixCode extends PanacheEntityBase {
 
     @Column(name = "AMOUNT")
     private BigDecimal amount;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public Long getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(Long transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public String getEmvqrcps() {
-        return emvqrcps;
-    }
-
-    public void setEmvqrcps(String emvqrcps) {
-        this.emvqrcps = emvqrcps;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
+    
 }
