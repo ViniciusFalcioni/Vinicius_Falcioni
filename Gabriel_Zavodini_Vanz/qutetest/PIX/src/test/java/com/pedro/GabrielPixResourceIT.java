4150b363e2b072423951f80236e993a114f7abba
Vinicius Falcioni <113529648+ViniciusFalcioni@users.noreply.github.com>
2023-12-01 19:35:09 -0300

    Update GabrielPixResourceIT.java

--- Gabriel_Zavodini_Vanz/qutetest/PIX/src/test/java/com/pedro/GabrielPixResourceIT.java
+++ Gabriel_Zavodini_Vanz/qutetest/PIX/src/test/java/com/pedro/GabrielPixResourceIT.java
@@ -3,6 +3,6 @@ package com.pedro;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class PedroPixResourceIT extends PedroPixResourceTest {
+public class GabrielPixResourceIT extends GabrielPixResourceTest {
     // Execute the same tests but in packaged mode.
 }
